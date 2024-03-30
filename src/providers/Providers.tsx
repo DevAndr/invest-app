8e7ff692399b101a5970bf90881a71685867ede2
Илья Носенко <pitbullstar96@gmail.com>
2024-03-30 22:41:29 +0300

    feat: added auth provider

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,21 +1,28 @@
-'use client'
+'use client';
 
 import React, {FC} from 'react';
 import {NextUIProvider} from "@nextui-org/react";
 import TitleAppBarProvider from "@/providers/TitleAppBar";
+import {ApolloProvider} from "@apollo/client";
+import apolloClient from "@/graphql/ApolloClient";
+import {AuthProvider} from '@/providers/AuthProvider';
 
 interface ProvidersProps {
-    children: React.ReactNode
+    children: React.ReactNode;
 }
 
 const Providers: FC<ProvidersProps> = ({children}) => {
     return (
         <NextUIProvider>
             <TitleAppBarProvider>
-                {children}
+                <ApolloProvider client={apolloClient}>
+                    <AuthProvider>
+                        {children}
+                    </AuthProvider>
+                </ApolloProvider>
             </TitleAppBarProvider>
         </NextUIProvider>
     );
-}
+};
 
 export default Providers;
