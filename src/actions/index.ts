2d31419383f43ca2591cc848a4592f44bef1ac6e
Илья Носенко <pitbullstar96@gmail.com>
2024-03-30 22:43:25 +0300

    fix: update actions and types

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,23 +1,73 @@
-import axios from "axios";
-import {Post, Profile} from "@/types";
+'use server';
 
-export async function requestAction() {
-    'use server'
-    const resp = await axios.get('https://jsonplaceholder.typicode.com/users');
+import instanceAxios from '@/axios/InstanceAxios';
+import {cookies} from 'next/headers';
+import {Post} from '@/types';
+import apolloClient from '@/graphql/ApolloClient';
+import {GET_POSTS} from '@/graphql/gql';
+import {FormCreatePostState} from '@/components/dialog/CreatePostDialog';
+
+
+export async function getMyPosts(): Promise<Post[]> {
+    const cookieStore = cookies();
+    const accessToken = cookieStore.get('accessToken');
+    console.log('getMyPosts', accessToken);
+    // const res = await instanceAxios.post<Post[]>('http://localhost:3030/graphql', {
+    //     query: `query Posts {
+    //     posts {
+    //         description
+    //         id
+    //         title
+    //     }
+    // }`
+    // });
+    // return res.data?.data?.posts;
+    const data = await apolloClient.query({query: GET_POSTS});
+    console.log(data);
+    // const res = await fetch('http://localhost:3030/graphql', {
+    //         credentials: 'include',
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             // 'Authorization': `Bearer ${accessToken}`
+    //         },
+    //         body: JSON.stringify({
+    //             query: `query Posts {
+    //             posts {
+    //                 description
+    //                 id
+    //                 title
+    //             }
+    //         }`
+    //         })
+    //     }
+    // );
+    //
+    // res.json().then(res => console.log(res));
+    return [];
 }
 
-export async function getArtist(index: number) {
-    const res = await axios(`https://jsonplaceholder.typicode.com/albums/${index}`)
-    return res.data
+export async function hasAccessToken() {
+    return !!cookies().get('accessToken');
 }
 
-export async function getPosts(): Promise<Post[]> {
-    const res = await axios<Post[]>(`https://jsonplaceholder.typicode.com/photos?start=0&end=30`)
-    return res.data;
-}
+export async function createCreatePostAction(prevState: any, formData: FormData) {
 
-export async function getProfile(id: string): Promise<Profile>{
-    // 'use server'
-    const res = await axios<Profile>(`https://jsonplaceholder.typicode.com/users/${id}`)
-    return res.data
+    const rawFormData = {
+        title: formData.get('title'),
+        description: formData.get('description'),
+    }
+
+    console.log(rawFormData, prevState);
+
+    return await new Promise(resolve => setTimeout(() => resolve({
+        ...rawFormData,
+        message: {
+            title: 'Введите название поста',
+            description: ''
+        },
+        isDone: false
+    }), 1000))
+
+
 }
